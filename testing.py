2c71dd7cbc56df641d2ecd9b8e0179fca054b214
JordanLaw <123150515+JordanLaw@users.noreply.github.com>
2026-01-28 10:46:01 +0800

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,72 @@
+import random
+
+def add(x, y):
+    z = x + y
+
+    return z
+
+def add2 (x1, y1):
+    z1 = add(30,40) + y1
+
+    return z1
+
+l = add2(30, 40)
+print(l)
+
+
+
+# x = 0
+# y = 40
+# z = x + y
+# print(z)
+#
+# add(30, 50)
+# add(50, 90)
+#
+# random_no = random.randrange(1, 100)
+# print("number: " + str(random_no))
+#
+#
+#
+# # x = 30
+# # y = 50
+# #
+# fruit = ["apple", "orange", "cherry", "123", "456"]
+#
+# fruit.append("pineapple")
+# # fruit.remove("apple")
+# # fruit.clear()
+#
+# # print("Length of list: " + fruit[0])
+#
+# # for x in fruit:
+# #     print(x)
+#
+# for i in range(4):
+#     print(fruit[i])
+#
+# while x < 50:
+#     x = x + 1
+#     print(x)
+#
+# #
+# # x,y,z = "apple", "orange", "cherry"
+# #
+# # print(type(y))
+# #
+# # print(x)
+# # print(fruit[0])
+# # print(y)
+# # print(fruit[1])
+# # print(z)
+# # print(fruit[2])
+#
+# # z = y + x
+# # print("z = " + str(z))
+# #
+# # print("Hello world")
+# #
+# # if y < x:
+# #     print("True")
+# # else:
+# #     print("False")
